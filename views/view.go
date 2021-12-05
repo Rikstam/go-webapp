6671aa109c688b1e6243209c27bbc22b7e773819
Riku <rikukestila@gmail.com>
2021-12-05 15:16:59 +0200

    >Add bootstrap layout

--- views/view.go
+++ views/view.go
@@ -3,8 +3,11 @@ package views
 import "html/template"
 
 // ... variadic parameter, takes any number of strings
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml",
+	)
 	//
 	t, err := template.ParseFiles(files...)
 
@@ -13,9 +16,11 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
