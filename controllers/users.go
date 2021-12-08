86f789e2bfa520025a90152e3b2e79ce2fd70f0c
Riku <rikukestila@gmail.com>
2021-12-08 11:47:26 +0200

    Use schema for form parsing form variables

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
+
 	"lenslocked.com/views"
 )
 
@@ -30,6 +32,16 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
+
+	decoder := schema.NewDecoder()
+	form := SignupForm{}
+	if err := decoder.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
 }
