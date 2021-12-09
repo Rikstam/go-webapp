73421cec03dc518305e9f5d95c820bb784b52cfd
Riku <rikukestila@gmail.com>
2021-12-09 12:23:19 +0200

    add template folder and extension via functions

--- views/view.go
+++ views/view.go
@@ -8,6 +8,7 @@ import (
 
 var (
 	LayoutDir         string = "views/layouts/"
+	TemplateDir       string = "views/"
 	TemplateExtension string = ".gohtml"
 )
 
@@ -21,6 +22,9 @@ func layoutFiles() []string {
 
 // ... variadic parameter, takes any number of strings
 func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExtension(files)
+
 	files = append(files, layoutFiles()...) // unpack the slice returned by layoutFiles()
 	//
 	t, err := template.ParseFiles(files...)
@@ -48,5 +52,17 @@ func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := v.Render(w, nil); err != nil {
 		panic(err)
+	}
+}
+
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+func addTemplateExtension(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExtension
 	}
 }
